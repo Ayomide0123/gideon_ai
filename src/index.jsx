4fec40f181299d8c452adf6213e69432a6e13153
Ayomide <oyetimehin31@gmail.com>
2023-10-20 16:23:51 +0100

    gideon

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 import Login from "./components/Login";
 import { store } from "./services/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HankoProfile from "./components/Profile";
 
 const EntryPoint = () => {
   return (
@@ -11,6 +12,7 @@ const EntryPoint = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/app" element={<App />} />
+          <Route path="/profile" element={<HankoProfile />} />
         </Routes>
       </BrowserRouter>
     </Provider>
