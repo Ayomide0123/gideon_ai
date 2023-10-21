1cac5bca2025542c9727fbc5a9c48e7019bd0d61
Ayomide <oyetimehin31@gmail.com>
2023-10-21 21:15:58 +0100

    gideon

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -166,7 +166,7 @@ const Demo = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Ayomide0123/gideon_ai", "_blank")
           }
           className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
