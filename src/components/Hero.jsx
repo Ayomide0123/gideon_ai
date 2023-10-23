21ff3653785a051b15c389b7ddf5de34cccdae73
Ayomide <oyetimehin31@gmail.com>
2023-10-23 11:15:50 +0100

    gideon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,8 @@ import Gideon from "../assets/gideon_icon.jpg";
 import LogoutBtn from "./LogoutBtn";
 import ProfileBtn from "./ProfileBtn";
 
+const hankoApi = import.meta.env.VITE_HANKO_API_URL;
+
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
@@ -15,16 +17,17 @@ const Hero = () => {
 
         <div className="flex flex-col">
           <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-            <ProfileBtn />
+            <ProfileBtn api={hankoApi} />
           </button>
           <button className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-            <LogoutBtn />
+            <LogoutBtn api={hankoApi} />
           </button>
         </div>
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with&nbsp; <br className="md:hidden" />
+        Summarize Articles with&nbsp;
+        <br className="md:hidden" />
         <span className="bg-blue-100 text-blue-800 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
           Gideon
         </span>
